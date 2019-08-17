9ed8ad2f235d95db464943be5f2bcf9da03fd346
Petrus Hoc <hoc081098@gmail.com>
2019-08-17 19:55:30 +0700

    Add chatbot UI

--- didauday_app/lib/src/app.dart
+++ didauday_app/lib/src/app.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Auth(),
+      home: Home(),
       routes: {
         '/auth/login': (context) => Login(),
         '/auth/register': (context) => Register(),
